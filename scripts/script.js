7815201c33750a07dd5932616084a87983cf59e8
Mikiyas-hs <mikiyashadis160@gmail.com>
2024-09-06 10:05:28 +0200

    visitekaart file
    visitekaart file gekopierd naar mijn your tribe profile card

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-// const expandButton = document.querySelector('header button') //Deze function zoekt naar het eerste element dat een knop (button) binnen een header (html)//
-//expandButton.addEventListener('click', expand) //dit zorgt ervoor dat er een functie word aangeroepen inprincipe dat de knop zich voldoet aan zijn functie//
-
-//function expand () {//deze functie laat eigenlijk bepalen wat er gebeurd als er op de knop word geklikt//
- //document.body.classList.toggle('expand') //dit zorgt ervoor dat de webpagina een bepaalde stijl krijgt door te zoeken naar de body in het HTML bestand//
-//} //
